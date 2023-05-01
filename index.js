49774095ca1850cef32057c031aa895952660e35
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 04:56:43 +0600

    get json file

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const allData =require('./data/allData.json');
+app.use(cors());
+app.get('/',(req, res)=>{
+        res.send('site is running')
+});
