bc8355bc67f154c17a8d5ea4f6103a275fdeab47
Deadless-Programmer <webtuhin@gmail.com>
2023-05-02 09:53:51 +0600

    dynamic data successfuly creaate

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-
 const allData =require('./data/allData.json');
+
 app.use(cors());
+
+
+
 app.get('/',(req, res)=>{
         res.send('site is running')
 });
 
+
+
 app.get('/allData', (req, res)=>{
     res.send(allData);
+})
+
+app.get('/allData/:id',(req, res)=>{
+    const id =req.params.id;
+    const data = allData.find(p=>p.id==id);
+    res.send(data)
 })
 
 app.listen(port, ()=>{
